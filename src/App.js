bd02dad42acf760e22278d74b0a986cc9e16bae2
Goncalo Ribeiro <goncalosribeiro@gmail.com>
2020-04-07 23:50:30 +0100

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,18 +1,88 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navigation from './components/Navigation';
 import Logo from './components/Logo';
 import Imagelink from './components/Imagelink';
+import FaceRecognition from './components/FaceRecognition';
+import Rank from './components/Rank';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Imagelink />
-      {/*<imageFaceRecognition />*/}
-    </div>
-  );
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100
+    },
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
+
+const app = new Clarifai.App({
+  apiKey: 'f3ed32b0386644a591556de6ed8145f3'
+ });
+
+class App extends Component {
+  constructor () {
+    super();
+    this.state={
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation =(data)=> {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = image.width;
+    const height = image.height;
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input 
+    )
+    .then( response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err=>console.log(err));
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}/>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <Imagelink onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Imagelink.js
+++ src/components/Imagelink.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Imagelink.css';
 
 
-const Imagelink = () => {
+const Imagelink = ({ onInputChange, onButtonSubmit }) => {
   return(
     <div>
       <p className='f3'>
@@ -10,8 +10,8 @@ const Imagelink = () => {
       </p>
       <div className='center'>
         <div className='form pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70' type='text' />
-          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple pointer'>Detect</button>
+          <input className='f4 pa2 w-70' type='text' onChange={onInputChange}/>
+          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple pointer' onClick={onButtonSubmit}>Detect</button>
         </div>
       </div>
       
